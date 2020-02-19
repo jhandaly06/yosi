a4eee4ba5f9856f86367681118e80a6b5537bdc8
jhandaly06 <60647184+jhandaly06@users.noreply.github.com>
2020-02-20 01:27:42 +0800

    Update _bahay.js

--- commands/Join System/_bahay.js
+++ commands/Join System/_bahay.js
@@ -13,10 +13,10 @@ var name=user.username ? user.username : user.first_name
 
 let mention="["+name+"]"+"("+"tg://user?id="+user.telegramid+")"
 if(user.telegramid==978337988){
-Bot.sendKeyboard("💲 Dashboard , \n🔽 Deposit, 🔼 Withdraw  ,\n🎌 Invite , 🔍 Extra , 💻 Calculator ,\n📞 Administrator,\n📮 Admin Panel 📮","*🏡 Mènu*")
+Bot.sendKeyboard("💲 Dashboard 💲, \n🔽 Deposit, 🔼 Withdraw  ,\n🎌 Invite , 🔍 Extra , 💻 Calculator ,\n📞 Administrator,\n📮 Admin Panel 📮","*🏡 Mènu*")
 }else{
 let user = User.getProperty("status");
 if (user=="member" | user =="administrator" | user=="creator"){
-Bot.sendKeyboard("💲 Dashboard , \n🔽 Deposit, 🔼 Withdraw  ,\n🎌 Invite ,💻 Calculator,\n📞 Administrator ","*🏡 Hello* "+mention+" *Welcome to Lady Lucks!*")
+Bot.sendKeyboard("💲 Dashboard 💲, \n🔽 Deposit, 🔼 Withdraw  ,\n🎌 Invite ,🔍 More, 💻 Calculator,\n📞 Administrator ","*🏡 Hello* "+mention+" *Welcome to Lady Lucks!*")
 }
 }
